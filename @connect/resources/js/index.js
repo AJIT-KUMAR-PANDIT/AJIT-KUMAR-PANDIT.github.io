669baa88b0d61d558a5e053a4b8f418d9511e7a8
AJIT KUMAR PANDIT <49229210+AJIT-KUMAR-PANDIT@users.noreply.github.com>
2023-08-22 23:57:11 +0530

    Add files via upload

--- @connect/resources/js/index.js
+++ @connect/resources/js/index.js
@@ -1,3 +1,5 @@
+// search 
+
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
@@ -12,5 +14,31 @@ function myFunction() {
         } else {
             li[i].style.display = "none";
         }
+    }
+}
+
+// custome  right mouse click
+
+document.onclick = hideMenu;
+document.oncontextmenu = rightClick;
+
+function hideMenu() {
+    document.getElementById(
+        "contextMenu").style.display = "none"
+}
+
+function rightClick(e) {
+    e.preventDefault();
+
+    if (document.getElementById(
+        "contextMenu").style.display == "block")
+        hideMenu();
+    else {
+        var menu = document
+            .getElementById("contextMenu")
+              
+        menu.style.display = 'block';
+        menu.style.left = e.pageX + "px";
+        menu.style.top = e.pageY + "px";
     }
 }
